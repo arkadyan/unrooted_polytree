3faaf98d499669c5ac99ed1ee9f1d29e2a0bf623
Matthew Shanley <matthewshanley@littlesecretsrecords.com>
2022-08-24 12:11:52 -0400

    feat: mix new unrooted_polytree

---
+++ lib/unrooted_polytree.ex
@@ -0,0 +1,18 @@
+defmodule UnrootedPolytree do
+  @moduledoc """
+  Documentation for `UnrootedPolytree`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UnrootedPolytree.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/unrooted_polytree_test.exs
@@ -0,0 +1,8 @@
+defmodule UnrootedPolytreeTest do
+  use ExUnit.Case
+  doctest UnrootedPolytree
+
+  test "greets the world" do
+    assert UnrootedPolytree.hello() == :world
+  end
+end
